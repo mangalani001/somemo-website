f0878188914bb51211f1b2e16f5957b0a7e0a641
mangalani001 <mangalanim@gmail.com>
2024-03-11 18:42:08 +0200

    similar to Jovian

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
-from flask import Flask, render_template
-from database import engine, load_all_xposts_from_db,load_yesterday_xposts_from_db, load_within_week_xposts_from_db, load_within_month_xposts_from_db, load_within_year_xposts_from_db 
+from flask import Flask, render_template, jsonify, request
+from database import engine, load_all_xposts_from_db,load_yesterday_xposts_from_db, load_within_week_xposts_from_db, load_within_month_xposts_from_db, load_within_year_xposts_from_db, load_jobs_from_db, load_job_from_db, add_application_to_db 
 
 app = Flask(__name__)
-
 
 
 @app.route("/")
@@ -15,7 +14,7 @@ def chart():
     return render_template('xpostchart.html')
 
 @app.route("/xposts")
-def hello_monitor():
+def get_xposts():
     dayposts = load_yesterday_xposts_from_db
     weekposts = load_within_week_xposts_from_db
     monthposts = load_within_month_xposts_from_db
@@ -23,6 +22,34 @@ def hello_monitor():
     allxposts = load_all_xposts_from_db
     return render_template('xitem.html', allxposts=allxposts, weekposts=weekposts, dayposts=dayposts, monthposts=monthposts, yearposts=yearposts, company_name='Somewa')
 
+@app.route("/api/jobs")
+def list_jobs():
+  jobs = load_jobs_from_db()
+  return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_job(id):
+  job = load_job_from_db(id)
+  
+  if not job:
+    return "Not Found", 404
+  
+  return render_template('jobpage.html', 
+                         job=job)
+
+@app.route("/api/job/<id>")
+def show_job_json(id):
+  job = load_job_from_db(id)
+  return jsonify(job)
+
+@app.route("/job/<id>/apply", methods=['post'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template('application_submitted.html', 
+                         application=data,
+                         job=job)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
--- database.py
+++ database.py
@@ -1,8 +1,8 @@
 from sqlalchemy import create_engine, Table, Column, text, String, MetaData, Integer, Computed
 import os
 
-#db_connection_string = "postgresql://fptdbqoy:G63T9sjGPWorNpmbpPGY91hXeCLBKe9-@bubble.db.elephantsql.com/fptdbqoy"
-db_connection_string = os.environ['DB_CONNECTION_STRING']
+db_connection_string = "postgresql://fptdbqoy:G63T9sjGPWorNpmbpPGY91hXeCLBKe9-@bubble.db.elephantsql.com/fptdbqoy"
+#db_connection_string = os.environ['DB_CONNECTION_STRING']
 
 engine = create_engine(db_connection_string)
 
@@ -45,3 +45,37 @@ def load_within_year_xposts_from_db():
         for row in result.all():
             posts.append(dict(row))
         return posts
+
+def load_jobs_from_db():
+  with engine.connect() as conn:
+    result = conn.execute(text("select * from jobs"))
+    jobs = []
+    for row in result.all():
+      jobs.append(dict(row))
+    return jobs    
+
+def load_job_from_db(id):
+  with engine.connect() as conn:
+    result = conn.execute(
+      text("SELECT * FROM jobs WHERE id = :val"),
+      val=id
+    )
+    rows = result.all()
+    if len(rows) == 0:
+      return None
+    else:
+      return dict(rows[0])
+
+
+def add_application_to_db(job_id, data):
+  with engine.connect() as conn:
+    query = text("INSERT INTO applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) VALUES (:job_id, :full_name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
+
+    conn.execute(query, 
+                 job_id=job_id, 
+                 full_name=data['full_name'],
+                 email=data['email'],
+                 linkedin_url=data['linkedin_url'],
+                 education=data['education'],
+                 work_experience=data['work_experience'],
+                 resume_url=data['resume_url'])        
